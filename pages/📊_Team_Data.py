d63b6aa0c6d864a07545473f1c8a8d9ddc3824da
Tyler Doiron <cdo@astuteinnovations.ca>
2025-04-30 17:30:39 -0300

    Update 📊_Team_Data.py

--- pages/📊_Team_Data.py
+++ pages/📊_Team_Data.py
@@ -235,5 +235,5 @@ for _, row in avg_height_by_year.iterrows():
 # Footer
 # -------------------------------
 st.markdown("---")
-st.caption("Advanced analytics powered by Streamlit • Crandall Chargers Volleyball © 2025")
+st.caption("Developed by Astute Innovations - Advanced analytics powered by Streamlit • Crandall Chargers Volleyball © 2025")
 st.markdown("---")
